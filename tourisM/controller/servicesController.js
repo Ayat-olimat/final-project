262c9b5653a1ec6a258c4756264be199e553d452
Ayat-olimat <82057557+Ayat-olimat@users.noreply.github.com>
2021-09-30 19:24:56 +0300

    Delete servicesController.js

--- tourisM/controller/servicesController.js
+++
@@ -1,5 +0,0 @@
-const detailsRoute =require('../routes/servicesRoute')
-const path= require('path')
-exports.getServices =(req,res,next)=>{
-res.render(path.join(__dirname,"..","View","services.ejs"))
-}
