94db89cc9df21d8874363a4a37dc137219467588
Ayat-olimat <82057557+Ayat-olimat@users.noreply.github.com>
2021-09-30 19:23:59 +0300

    Delete servicesRoute.js

--- tourisM/routes/servicesRoute.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require('express');
-const express= require('express');
-const router= express.Router();
-const servicesController  = require("../controller/servicesController");
-router.get('/services',servicesController.getServices)
-module.exports=router;
