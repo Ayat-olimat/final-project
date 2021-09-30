1177d5d933fb9689d233afbb6cbc8b746e852684
Ayat-olimat <Ayatolemat@gmail.com>
2021-09-30 19:20:55 +0300

    new-edit

--- tourisM/controller/AqapaController.js
+++
@@ -1,5 +0,0 @@
-const Aaaparoute =require('../routes/AqapaRoute')
-const path= require('path')
-exports.getAqapa =(req,res,next)=>{
-res.render(path.join(__dirname,"..","View","Aqapa.ejs"))
-}
--- tourisM/routes/AqapaRoute.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require('express');
-const express= require('express');
-const router= express.Router();
-const AqapaController = require("../controller/AqapaController");
-router.get('/Aqapa',AqapaController.getAqapa)
-module.exports=router;
