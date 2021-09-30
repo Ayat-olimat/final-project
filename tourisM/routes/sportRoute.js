d36a8c373306fe14991f5ee280c959d671290223
Ayat-olimat <82057557+Ayat-olimat@users.noreply.github.com>
2021-09-30 19:23:45 +0300

    Delete sportRoute.js

--- tourisM/routes/sportRoute.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require('express');
-const express= require('express');
-const router= express.Router();
-const sportController = require("../controller/sportController");
-router.get('/sport',sportController.getsport)
-module.exports=router;
