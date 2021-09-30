70e06d4be294a0753270144e5ec3d0a23129dcaf
Ayat-olimat <82057557+Ayat-olimat@users.noreply.github.com>
2021-09-30 19:24:46 +0300

    Delete GalleryController.js

--- tourisM/controller/GalleryController.js
+++
@@ -1,5 +0,0 @@
-const detailsRoute =require('../routes/GalleryRoute')
-const path= require('path')
-exports.getGallery =(req,res,next)=>{
-res.render(path.join(__dirname,"..","View","Gallery.ejs"))
-}
