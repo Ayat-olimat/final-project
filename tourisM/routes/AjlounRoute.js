6a5287607c48985d0fb3276e611df392dd6896f9
Ayat-olimat <82057557+Ayat-olimat@users.noreply.github.com>
2021-09-30 19:23:30 +0300

    Delete AjlounRoute.js

--- tourisM/routes/AjlounRoute.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require('express');
-const express= require('express');
-const router= express.Router();
-const AjlounController = require("../controller/AjlounController");
-router.get('/Ajloun',AjlounController.getAjloun)
-module.exports=router;
